482fccf2668ed7d7b912e78ae5aaaecb6ac8091a
Anna.Rubchenia <anna.rub4enya@yandex.ru>
2022-10-17 16:35:42 +0300

    added new component Burger

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 //@ts-ignore
 import styles from "./App.module.css"
 import Title from "./Components/Title";
+import  Burger from "./Components/Burger"
 
 const App = () => {
   return (
     <div className ={styles.container}>
       <Title title={"Sign In"} /> 
+      <Burger status={true} onClick={() => {}}/>
     </div>
   );
 }
