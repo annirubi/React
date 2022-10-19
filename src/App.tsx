608544735c03ec12ba3aaaa7718c0acf0762dcdc
Anna.Rubchenia <anna.rub4enya@yandex.ru>
2022-10-19 17:02:45 +0300

    created Burger component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 //@ts-ignore
 import styles from "./App.module.css"
 import Title from "./Components/Title";
-import  Burger from "./Components/Burger"
+// import  Burger from "./Components/Burger"
+import { BurgerClosedIcon, BurgerOpenedIcon } from "./Assets/icons";
+import Button, { ButtonTypes } from "./Components/Button";
 
 const App = () => {
+  const [isOpened, setOpened] = useState(false)
   return (
     <div className ={styles.container}>
       <Title title={"Sign In"} /> 
-      <Burger status={true} onClick={() => {}}/>
+      <Button
+        title={!isOpened ? <BurgerClosedIcon /> : <BurgerOpenedIcon />}
+        type={ButtonTypes.Primary}
+        className={styles.burgerButton}
+        onClick={() => setOpened(!isOpened)}
+      />
     </div>
   );
 }
